b25c485e35fcecaa6b751dbe360311bd5429efbf
Yan Huo <yan.huo@tufts.edu>
2017-01-03 14:28:54 -0500

    change format

--- checkIfTooMuch/app.js
+++ checkIfTooMuch/app.js
@@ -1,25 +1,25 @@
 (function () {
-	'use strict';
-	angular.module('LunchCheck', [])
-	.controller('LunchCheckController', LunchCheckController);
-	LunchCheckController.$inject = ['$scope'];
+  'use strict';
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject = ['$scope'];
 
-	function LunchCheckController($scope) {
-		$scope.input = "";
-		$scope.message = "";
-		$scope.checkIfTooMuch = function () {
-			if ($scope.input == "") {
-				$scope.message = "Please enter data first";
-			}
-			else {
-				var count = $scope.input.split(',').length;
-				if (count <= 3) {
-					$scope.message = "Enjoy!";
-				}
-				else {
-					$scope.message = "Too Much!";
-				}
-			}
-		};
-	}
+  function LunchCheckController($scope) {
+    $scope.input = "";
+    $scope.message = "";
+    $scope.checkIfTooMuch = function () {
+      if ($scope.input == "") {
+        $scope.message = "Please enter data first";
+      }
+      else {
+        var count = $scope.input.split(',').length;
+        if (count <= 3) {
+          $scope.message = "Enjoy!";
+        }
+        else {
+          $scope.message = "Too Much!";
+        }
+      }
+    };
+  }
 })();
