036e13a4d10271000a931391392ab4669abf2f32
Yan Huo <yan.huo@tufts.edu>
2017-01-09 11:13:51 -0500

    add change color

--- checkIfTooMuch/app.js
+++ checkIfTooMuch/app.js
@@ -7,17 +7,22 @@
   function LunchCheckController($scope) {
     $scope.input = "";
     $scope.message = "";
+    $scope.customStyle = {color : "black"};
+
     $scope.checkIfTooMuch = function () {
       if ($scope.input == "") {
         $scope.message = "Please enter data first";
+        $scope.customStyle.color = "red";
       }
       else {
         var count = $scope.input.split(',').length;
         if (count <= 3) {
           $scope.message = "Enjoy!";
+          $scope.customStyle.color = "green";
         }
         else {
           $scope.message = "Too Much!";
+          $scope.customStyle.color = "Orange";
         }
       }
     };
